8a1005858770b6fe7f9d2c3b4104d8a18edda103
Tomasz Mikolajewicz <morhendos@gmail.com>
2024-12-19 18:42:23 +0200

    Add custom error types

--- lib/errors.ts
+++ lib/errors.ts
@@ -1,36 +1,39 @@
-export class PDFProcessingError extends Error {
-  constructor(
-    message: string,
-    public readonly code: PDFErrorCode,
-    public readonly originalError?: unknown
-  ) {
-    super(message);
-    this.name = 'PDFProcessingError';
-  }
-}
-
-export type PDFErrorCode = 
+export type ErrorCode =
   | 'EMPTY_FILE'
   | 'CORRUPT_FILE'
   | 'NO_TEXT_CONTENT'
   | 'EXTRACTION_ERROR'
-  | 'INVALID_FORMAT';
+  | 'INVALID_FORMAT'
+  | 'API_ERROR'
+  | 'INVALID_INPUT'
+  | 'TEXT_PROCESSING_ERROR'
+  | 'UNKNOWN_ERROR'
+  | 'CONFIGURATION_ERROR';
 
-export class ContractAnalysisError extends Error {
-  constructor(
-    message: string,
-    public readonly code: ContractAnalysisErrorCode,
-    public readonly originalError?: unknown
-  ) {
+export class BaseError extends Error {
+  constructor(message: string, public code: ErrorCode, public cause?: unknown) {
     super(message);
-    this.name = 'ContractAnalysisError';
+    this.name = this.constructor.name;
+    Error.captureStackTrace(this, this.constructor);
   }
 }
 
-export type ContractAnalysisErrorCode = 
-  | 'PDF_PROCESSING_ERROR'
-  | 'TEXT_PROCESSING_ERROR'
-  | 'API_ERROR'
-  | 'INVALID_INPUT'
-  | 'CONFIGURATION_ERROR'
-  | 'UNKNOWN_ERROR';
+export class PDFProcessingError extends BaseError {
+  constructor(
+    message: string,
+    code: Extract<ErrorCode, 'EMPTY_FILE' | 'CORRUPT_FILE' | 'NO_TEXT_CONTENT' | 'EXTRACTION_ERROR' | 'INVALID_FORMAT'>,
+    cause?: unknown
+  ) {
+    super(message, code, cause);
+  }
+}
+
+export class ContractAnalysisError extends BaseError {
+  constructor(
+    message: string,
+    code: Extract<ErrorCode, 'API_ERROR' | 'INVALID_INPUT' | 'TEXT_PROCESSING_ERROR' | 'UNKNOWN_ERROR' | 'CONFIGURATION_ERROR'>,
+    cause?: unknown
+  ) {
+    super(message, code, cause);
+  }
+}
