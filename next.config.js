375783a3e0f44b8846133ee3d33cc1e66b0429f5
Tomasz Mikolajewicz <morhendos@gmail.com>
2024-12-19 19:38:05 +0200

    Restore to last working state before Sentry integration

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false,
-      }
-    }
-    return config
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
