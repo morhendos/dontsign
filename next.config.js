9a2bf5021187f758f05af5de40839dfd92030577
Tomasz Mikolajewicz <morhendos@gmail.com>
2024-12-19 19:13:11 +0200

    Update next.config.js for PDF.js compatibility

--- next.config.js
+++ next.config.js
@@ -1,34 +1,35 @@
 const { withSentryConfig } = require("@sentry/nextjs");
 
 const nextConfig = {
-  // your existing next config
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
+  },
 };
 
 const sentryWebpackPluginOptions = {
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
-
   org: process.env.SENTRY_ORG,
   project: process.env.SENTRY_PROJECT,
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
 
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  // Upload a larger set of source maps for prettier stack traces
   widenClientFileUpload: true,
 
-  // Automatically annotate React components to show their full name in breadcrumbs and session replay
+  // Automatically annotate React components
   reactComponentAnnotation: {
     enabled: true,
   },
 
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers
+  // Route browser requests to Sentry through a Next.js rewrite
   tunnelRoute: "/monitoring",
 
   // Hides source maps from generated client bundles
   hideSourceMaps: true,
 
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  // Automatically tree-shake Sentry logger statements
   disableLogger: true,
 
   // Enables automatic instrumentation of Vercel Cron Monitors
