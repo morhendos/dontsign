822556557401eae6bb9912677f1cc9dd05fa393f
Tomasz Mikolajewicz <morhendos@gmail.com>
2024-12-20 17:38:38 +0200

    fix: wrap Analytics component in Suspense

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Suspense } from 'react'
 import { Analytics } from '@/components/Analytics';
 
 const inter = Inter({ subsets: ['latin'] })
@@ -18,7 +19,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
-        <Analytics />
+        <Suspense>
+          <Analytics />
+        </Suspense>
       </body>
     </html>
   )
