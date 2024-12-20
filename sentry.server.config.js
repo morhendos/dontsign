6ad5b3598e8fda78871b371f493e84cee05b0153
Tomasz Mikolajewicz <morhendos@gmail.com>
2024-12-20 08:42:27 +0200

    Update Sentry config to use environment variables

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -1,13 +1,16 @@
 // This file configures the initialization of Sentry on the server side
 import * as Sentry from "@sentry/nextjs";
 
+const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
+
 Sentry.init({
+  dsn: SENTRY_DSN,
   // Adjust this value in production
   tracesSampleRate: 0.1,
-  
-  // Only enable in production
+
   enabled: process.env.NODE_ENV === 'production',
+  environment: process.env.NEXT_PUBLIC_ENV || process.env.NODE_ENV,
   
-  // Disable the default integrations
+  // Disable the default integrations until we explicitly want to use them
   defaultIntegrations: false,
 });
