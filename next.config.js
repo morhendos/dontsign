bdff771f0f58ec395c2c8e6fb2071ba6237193d4
Tomasz Mikolajewicz <morhendos@gmail.com>
2024-12-20 16:29:02 +0200

    fix: simplify Sentry config to focus on sourcemaps issue

--- next.config.js
+++ next.config.js
@@ -10,32 +10,11 @@ const nextConfig = {
       canvas: false
     };
     return config;
-  },
-  // Disable source maps in production
-  productionBrowserSourceMaps: false
+  }
 }
 
+// Make sure adding Sentry options is the last code to run before exporting
 module.exports = withSentryConfig(
   nextConfig,
-  {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
-
-    // Suppresses source map uploading logs during build
-    silent: true,
-
-    org: process.env.SENTRY_ORG,
-    project: process.env.SENTRY_PROJECT,
-
-    // Enables automatic instrumentation of Next.js server functions
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/
-    autoInstrumentServerFunctions: true,
-
-    // Upload sourcemaps to Sentry but hide them from browser
-    hideSourceMaps: true,
-
-    // Settings for performance monitoring
-    tracesSampleRate: 0.1,
-    tracingOrigins: ["localhost", /^\//, process.env.NEXT_PUBLIC_VERCEL_URL],
-  }
+  { hideSourceMaps: true }
 );
