8a7c54b5e98001eb81d083e5f1b43d50507a8ef1
Tomasz Mikolajewicz <morhendos@gmail.com>
2024-12-20 08:46:37 +0200

    Enable basic Sentry error monitoring on server

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -1,16 +1,26 @@
-// This file configures the initialization of Sentry on the server side
 import * as Sentry from "@sentry/nextjs";
 
 const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
   dsn: SENTRY_DSN,
-  // Adjust this value in production
+  
   tracesSampleRate: 0.1,
-
   enabled: process.env.NODE_ENV === 'production',
   environment: process.env.NEXT_PUBLIC_ENV || process.env.NODE_ENV,
   
-  // Disable the default integrations until we explicitly want to use them
-  defaultIntegrations: false,
+  // Enable node specific integrations
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Sentry.Integrations.OnUncaughtException(),
+    new Sentry.Integrations.OnUnhandledRejection(),
+  ],
+
+  // Don't send errors in development
+  beforeSend(event) {
+    if (process.env.NODE_ENV === 'development') {
+      return null;
+    }
+    return event;
+  },
 });
