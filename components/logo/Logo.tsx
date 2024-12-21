c3dea67daf8f9e24a776be01134d1602701a7a5e
Tomasz Mikolajewicz <morhendos@gmail.com>
2024-12-22 00:01:15 +0200

    refactor: simplify Logo component with external SVG reference

--- components/logo/Logo.tsx
+++ components/logo/Logo.tsx
@@ -1,24 +1,23 @@
 'use client';
 
-import { LogoIcon } from './LogoIcon';
-
 interface LogoProps {
   className?: string;
 }
 
 export function Logo({ className = '' }: LogoProps) {
   return (
-    <svg 
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 211.4 26.208"
-      className={`w-auto text-gray-900 dark:text-white transition-colors duration-200 ${className}`}
-      aria-label="dontSign.ai logo"
-      role="img"
-    >
-      <title>dontSign.ai</title>
-      <g className="fill-current">
-        <LogoIcon />
-      </g>
-    </svg>
+    <div className={`relative w-[211.4px] h-[26.208px] ${className}`}>
+      <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        width="211.4"
+        height="26.208"
+        viewBox="0 0 211.4 26.208"
+        className="text-gray-900 dark:text-white"
+        preserveAspectRatio="xMidYMid meet"
+        aria-label="dontSign.ai logo"
+      >
+        <use href="/images/logo.svg#logo" className="fill-current" />
+      </svg>
+    </div>
   );
 }
