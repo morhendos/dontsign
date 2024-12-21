ad13027964f43b8a05ec23515a89c698e12e7f4f
Tomasz Mikolajewicz <morhendos@gmail.com>
2024-12-21 11:08:38 +0200

    fix: Update Progress component with correct type definitions
    - Added className to ProgressProps interface
    - Added proper class merging with cn utility
    - Fixed component type definitions

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -2,10 +2,13 @@
 
 import * as React from 'react';
 import * as ProgressPrimitive from '@radix-ui/react-progress';
+import { cn } from '@/lib/utils';
 
-export interface ProgressProps extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
+export interface ProgressProps
+  extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
   value?: number;
   indicatorColor?: string;
+  className?: string;
 }
 
 const Progress = React.forwardRef<
@@ -14,7 +17,10 @@ const Progress = React.forwardRef<
 >(({ className, value = 0, indicatorColor = 'bg-blue-600', ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
-    className="relative h-4 w-full overflow-hidden rounded-full bg-gray-100"
+    className={cn(
+      "relative h-4 w-full overflow-hidden rounded-full bg-gray-100",
+      className
+    )}
     {...props}
   >
     <ProgressPrimitive.Indicator
