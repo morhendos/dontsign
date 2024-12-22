f52074e7d99628e32b6d4fa1c1675fba9a1f9b99
Tomasz Mikolajewicz <morhendos@gmail.com>
2024-12-23 01:32:06 +0200

    Fix import path for FileUploadArea component

---
+++ components/hero/index.tsx
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+import { Button } from '@/components/ui/button';
+import { FileUploadArea } from '@/components/contract-upload/FileUploadArea';
+
+// Rest of the component code...
