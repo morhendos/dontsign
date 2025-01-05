0b2042fc0e7d1c4f6df7d6271e5d7a7223e7d2e0
Tomasz Mikolajewicz <morhendos@gmail.com>
2025-01-05 17:29:09 +0200

    refactor: update main page to use ContractAnalyzer component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,5 @@
-import Hero from '@/components/hero/Hero'
-import HowItWorks from '@/components/how-it-works'
-import KeyFeatures from '@/components/key-features'
+import { ContractAnalyzer } from '@/components/contract-analyzer';
 
 export default function Home() {
-  return (
-    <>
-      <Hero />
-      <HowItWorks />
-      <KeyFeatures />
-    </>
-  )
+  return <ContractAnalyzer />;
 }
