b362eb7fe88c168357d7fd05682ff5422c9e3e49
Tomasz Mikolajewicz <morhendos@gmail.com>
2025-01-07 01:23:11 +0200

    fix: remove useAnalysisLog export as it moved to contract-analyzer

--- components/analysis-log/index.ts
+++ components/analysis-log/index.ts
@@ -1,4 +1,2 @@
 export { AnalysisLog } from './AnalysisLog';
 export type { LogEntry } from './AnalysisLog';
-export { useAnalysisLog } from './useAnalysisLog';
-export type { AnalysisLogState } from './useAnalysisLog';
