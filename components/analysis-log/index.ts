d7d0d0d7ca774bf59a0f4bcb03954c7d2933cce6
Tomasz Mikolajewicz <morhendos@gmail.com>
2025-01-07 01:22:08 +0200

    fix: change AnalysisLog export to named export to match component definition

--- components/analysis-log/index.ts
+++ components/analysis-log/index.ts
@@ -1,4 +1,4 @@
-export { default } from './AnalysisLog';
+export { AnalysisLog } from './AnalysisLog';
 export type { LogEntry } from './AnalysisLog';
 export { useAnalysisLog } from './useAnalysisLog';
 export type { AnalysisLogState } from './useAnalysisLog';
