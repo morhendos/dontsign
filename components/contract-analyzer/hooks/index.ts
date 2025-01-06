bdc6bac34e671e634c1d943b8986e63a917b9cf5
Tomasz Mikolajewicz <morhendos@gmail.com>
2025-01-06 11:58:39 +0200

    fix: add and update all index files for proper exports

--- components/contract-analyzer/hooks/index.ts
+++ components/contract-analyzer/hooks/index.ts
@@ -1,6 +1,6 @@
-// Re-export all hooks
 export * from './analysis';
 export * from './file';
 export * from './state';
 export * from './storage';
 export * from './ui';
+export * from './useContractAnalyzer';
