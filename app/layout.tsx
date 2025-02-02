76f5b99acaca4f2f0cc644b4f74cf220ac2b8b94
Tomasz Mikolajewicz <morhendos@gmail.com>
2025-02-02 23:42:50 +0200

    Add Microsoft Clarity script (#130)
    * Add Microsoft Clarity script
    
    * Use environment variable for Clarity ID
    
    * Add Clarity ID to environment variables example
    
    * Add Clarity ID to existing env example

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,17 @@ export default function RootLayout({
             gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
           `}
         </Script>
+        
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}");
+          `}
+        </Script>
       </head>
       <body className={inter.className}>
         <ThemeProvider
